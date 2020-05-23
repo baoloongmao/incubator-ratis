b986961242435079085c27544ccb98ce5829c609
runzhiwang <runzhiwang@tencent.com>
2020-05-23 23:56:42 +0800

    1

--- ratis-logservice/src/main/java/org/apache/ratis/logservice/server/MetaStateMachine.java
+++ ratis-logservice/src/main/java/org/apache/ratis/logservice/server/MetaStateMachine.java
@@ -114,12 +114,12 @@ public class MetaStateMachine extends BaseStateMachine {
       this.metadataGroupId = metadataGroupId;
       this.logServerGroupId = logServerGroupId;
       this.failureDetectionPeriod = failureDetectionPeriod;
-        //RaftClientConfigKeys.Rpc.setRequestTimeout(properties, TimeDuration.valueOf(15, TimeUnit.SECONDS));
+        RaftClientConfigKeys.Rpc.setRequestTimeout(properties, TimeDuration.valueOf(15, TimeUnit.SECONDS));
     }
 
     @Override
     public void initialize(RaftServer server, RaftGroupId groupId, RaftStorage storage) throws IOException {
-        //RaftClientConfigKeys.Rpc.setRequestTimeout(properties, TimeDuration.valueOf(15, TimeUnit.SECONDS));
+        RaftClientConfigKeys.Rpc.setRequestTimeout(properties, TimeDuration.valueOf(15, TimeUnit.SECONDS));
         this.raftServer = server;
         this.metricRegistry = LogServiceMetricsRegistry
             .createMetricRegistryForLogServiceMetaData(server.getId().toString());
