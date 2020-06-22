53a050707f651f899eb3a6c49d69f9c8745d6ec9
runzhiwang <runzhiwang@tencent.com>
2020-06-22 19:01:09 +0800

    fix unsteady

--- ratis-server/src/main/java/org/apache/ratis/server/impl/RaftServerImpl.java
+++ ratis-server/src/main/java/org/apache/ratis/server/impl/RaftServerImpl.java
@@ -824,7 +824,6 @@ public class RaftServerImpl implements RaftServerProtocol, RaftServerAsynchronou
         final boolean termUpdated = changeToFollower(candidateTerm, true, "recognizeCandidate:" + candidateId);
         // see Section 5.4.1 Election restriction
         if (state.isLogUpToDate(candidateLastEntry) && fs != null) {
-          fs.updateLastRpcTime(FollowerState.UpdateType.REQUEST_VOTE);
           state.grantVote(candidateId);
           voteGranted = true;
         }
@@ -832,7 +831,9 @@ public class RaftServerImpl implements RaftServerProtocol, RaftServerAsynchronou
           state.persistMetadata(); // sync metafile
         }
       }
-      if (!voteGranted && shouldSendShutdown(candidateId, candidateLastEntry)) {
+      if (voteGranted) {
+        fs.updateLastRpcTime(FollowerState.UpdateType.REQUEST_VOTE);
+      } else if(shouldSendShutdown(candidateId, candidateLastEntry)) {
         shouldShutdown = true;
       }
       reply = ServerProtoUtils.toRequestVoteReplyProto(candidateId, getMemberId(),
