11c13234f1b98aa4373f12246555be123acbc404
runzhiwang <runzhiwang@tencent.com>
2020-08-02 14:48:11 +0800

    remove unrelated test

---
+++ commit.sh
@@ -0,0 +1,37 @@
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+git commit --allow-empty -m "1"
+git push origin print-log-testBasicInstallSnapshot
+
